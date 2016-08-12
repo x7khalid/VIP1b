ad07e1bbd353810b2b145cc3b11207140713c497
Khaled_Craft1b <iiBa4x@gmail.com>
2016-08-12 17:53:06 +0300

    Update Main.php

--- src/iBa4x/VIP/Main.php
+++ src/iBa4x/VIP/Main.php
@@ -72,7 +72,7 @@ class Main extends PluginBase implements Listener{
           $level = $this->getConfig()->get("Level");
           $player = $event->getPlayer();
           if($player->hasPermission("vip1b.move.vip")){
-            $player->sendPopup(T::YELLOW . "Welcom Room VIP " . T::GREEN . $player->getName());
+            $player->sendPopup(T::YELLOW . "Welcome Room VIP " . T::GREEN . $player->getName());
           }else{
             $player->teleport(new Vector3($x, $y+1, $z, $level));
             $player->sendPopup(T::YELLOW . "$pop");
