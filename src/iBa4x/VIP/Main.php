dd48d0cbe8710094700ef04f3d2be0374f585210
Khaled_Craft1b <iiBa4x@gmail.com>
2016-09-05 15:11:59 +0300

    Update Main.php

--- src/iBa4x/VIP/Main.php
+++ src/iBa4x/VIP/Main.php
@@ -1,6 +1,6 @@
 <?php
 namespace iBa4x\VIP;
-
+#this plugin by iBa4x
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
@@ -10,7 +10,6 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
 use pocketmine\utils\TextFormat;
 class Main extends PluginBase implements Listener{
-  public $permission = "vip1b.area.vip";
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info(TextFormat::GREEN . "By iBa4x");
@@ -43,7 +42,7 @@ class Main extends PluginBase implements Listener{
     						$sender->sendMessage(TextFormat::GREEN."- Pos2 set to: ($x, $y, $z)");
     					return true;
     					case "permission":
-    						$sender->sendMessage(TextFormat::YELLOW."- $permission");
+    						$sender->sendMessage(TextFormat::YELLOW."- vip1b.area.vip");
     					return true;
     				}
     			}
@@ -62,7 +61,7 @@ class Main extends PluginBase implements Listener{
 	$y2 = $this->getConfig()->get("y2");
 	$z2 = $this->getConfig()->get("z2");
 	if($this->isInside($player,new Vector3($x1,$y1,$z1),new Vector3($x2,$y2,$z2))){
-		if(!$player->hasPermission($permission)){
+		if(!$player->hasPermission("vip1b.area.vip")){
 			$spawn = $this->getServer()->getDefaultLevel()->getSafeSpawn();
 			$player->teleport(new Vector3($spawn->getX(),$spawn->getY(),$spawn->getZ()));
 			$player->sendMessage(TextFormat::RED."You do not have permission to join area");
